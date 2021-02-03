138716575b1979fef905723d015efeb0fc3d3c0a
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-02-02 22:46:26 -0300

    feat: Add first-section to home content

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainHomeComponentComponent } from './main-home-component/main-home-component.component';
+import { ParallaxDirective } from '../config/Parallax.directive';
 
 
 
 @NgModule({
-  declarations: [MainHomeComponentComponent],
+  declarations: [
+    MainHomeComponentComponent,
+    ParallaxDirective
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
--- src/app/home/main-home-component/main-home-component.component.ts
+++ src/app/home/main-home-component/main-home-component.component.ts
@@ -10,14 +10,49 @@ import { HttpClientService } from 'src/app/config/HttpClientService.service';
 export class MainHomeComponentComponent implements OnInit {
 
   constructor(private readonly httpClientService: HttpClientService) { }
-  public pokemonResult: PokemonResultDTO[];
+
+  public dataDescritive: string;
+  public dataTitle: string;
+  public dataSubtitle: string;
 
   ngOnInit(): void {
+    const title: string = "PSICÓLOGO";
+    const subTitle: string = "Um(a) Psicólogo(a) para orientá-lo";
+    const data: string = `
+      Você está vivenciando questões relacionadas a um trauma, ao sofrimento, ao vício,
+      à ansiedade ou a grandes mudanças de vida? Chega um momento em que a ajuda de um
+      profissional certificado é necessária para lidarmos com muitos dos desafios da vida.
+      Sou um(a) Psicólogo(a) licenciado(a) em São Paulo e estou aqui para apoiar você na sua
+      jornada de cura e auto-descoberta.
+    `;
+
+    this.setDataTitle(title);
+    this.setDataSubTitle(subTitle);
+    this.setDataDescritive(data);
+
+    /*
     this.httpClientService.getPokemon("https://pokeapi.co/api/v2/pokemon")
     .subscribe((data: PokemonApiDTO) => {
       this.pokemonResult = data.results
       console.log(this.pokemonResult)
     });
+    */
+  }
+
+  public getParallaxImageUrl: () => String = () => {
+    return "url('../../../assets/TeraphyImg.jpg')";
+  }
+
+  private setDataDescritive: (value: string) => void = (value: string) => {
+    this.dataDescritive = value;
+  }
+
+  private setDataTitle: (value: string) => void = (value: string) => {
+    this.dataTitle = value;
+  }
+
+  private setDataSubTitle: (value: string) => void = (value: string) => {
+    this.dataSubtitle = value;
   }
 
 }

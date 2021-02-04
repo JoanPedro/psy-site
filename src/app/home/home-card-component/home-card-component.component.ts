87de40da6d6997117c8bf756f77a49d3a65745a4
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-02-04 10:44:46 -0300

    feat: Turns CardComponent integrated with Router-Based Component ShedulingEachPage

--- src/app/home/home-card-component/home-card-component.component.ts
+++ src/app/home/home-card-component/home-card-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home-card-component',
@@ -6,14 +7,39 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./home-card-component.component.css']
 })
 export class HomeCardComponentComponent implements OnInit {
+
   @Input() title = '';
   @Input() imageUrl = '';
   @Input() hour = '';
   @Input() price = '';
 
-  constructor() { }
+  @Input() detailUrl = 'agenda';
+
+  @Input() otherImageUrl = '';
+  @Input() mainContentTitle = '';
+  @Input() mainContentBaseHour = '';
+  @Input() mainContentBasePrice = '';
+  @Input() descriptionContentServiceDescription = '';
+  @Input() aboutContactInformationEmailDescription = '';
+
+  constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public onClick: () => void = () => {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        otherImageUrl: this.otherImageUrl,
+        mainContentTitle: this.mainContentTitle,
+        mainContentBaseHour: this.mainContentBaseHour,
+        mainContentBasePrice: this.mainContentBasePrice,
+        descriptionContentServiceDescription: this.descriptionContentServiceDescription,
+        aboutContactInformationEmailDescription: this.aboutContactInformationEmailDescription
+      }
+    };
+
+    this.router.navigate([this.detailUrl], navigationExtras);
   }
 
 }

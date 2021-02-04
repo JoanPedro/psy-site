75ab7833f217c32746400fbb1b0598dda74b836e
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-02-04 10:43:57 -0300

    feat: Add dynamic routes for new integration CardComponent

--- src/app/scheduling/scheduling-routing.module.ts
+++ src/app/scheduling/scheduling-routing.module.ts
@@ -1,10 +1,15 @@
+import { SchedulingEachPageComponentComponent } from './scheduling-each-page-component/scheduling-each-page-component.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainSchedulingComponentComponent } from './main-scheduling-component/main-scheduling-component.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainSchedulingComponentComponent}
+  { path: '', component: MainSchedulingComponentComponent},
+  ...['1', '2', '3'].map((path: string) => ({
+    path,
+    component: SchedulingEachPageComponentComponent
+  }))
 ];
 
 @NgModule({
